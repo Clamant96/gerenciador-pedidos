ec7193c252015bc9238eba7d6c61f17bb3bd5970
Kevin Alec Neri Lazzarotto <kevin.lazzarotto@hotmail.com>
2022-07-26 22:38:41 -0300

    Postagem de projeto para build

--- src/main/java/br/com/helpconnect/gerenciadorpedidos/controller/CategoriaController.java
+++ src/main/java/br/com/helpconnect/gerenciadorpedidos/controller/CategoriaController.java
@@ -26,6 +26,7 @@ public class CategoriaController {
 	@Autowired
 	private CategoriaRepository repository;
 	
+	// TESTE
 	@GetMapping
 	public ResponseEntity<List<Categoria>> findAllCategorias() {
 		
