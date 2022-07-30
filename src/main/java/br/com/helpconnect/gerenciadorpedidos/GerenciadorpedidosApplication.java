4fa864cfcf4373090609a6cc01edeee9aeacf0e3
Kevin Alec Neri Lazzarotto <kevin.lazzarotto@hotmail.com>
2022-07-30 09:23:25 -0300

    Deploy automatico com Jenkins e tomcat 8

--- src/main/java/br/com/helpconnect/gerenciadorpedidos/GerenciadorpedidosApplication.java
+++ src/main/java/br/com/helpconnect/gerenciadorpedidos/GerenciadorpedidosApplication.java
@@ -2,9 +2,10 @@ package br.com.helpconnect.gerenciadorpedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GerenciadorpedidosApplication {
+public class GerenciadorpedidosApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GerenciadorpedidosApplication.class, args);
